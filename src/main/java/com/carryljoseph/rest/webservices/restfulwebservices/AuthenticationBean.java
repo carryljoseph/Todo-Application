5671555a885d444dbf5f3c0a57d26585972b2914
Carol Joseph <carryljoseph@gmail.com>
2019-08-17 16:18:54 +0530

    Initial Java commit

---
+++ src/main/java/com/carryljoseph/rest/webservices/restfulwebservices/AuthenticationBean.java
@@ -0,0 +1,21 @@
+package com.carryljoseph.rest.webservices.restfulwebservices;
+
+public class AuthenticationBean {
+	
+	private String message;
+	
+	public AuthenticationBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+	
+}
